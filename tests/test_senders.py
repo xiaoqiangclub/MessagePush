aa0830c36fc25c931629c9474bdfdb67494adc36
祖国滴°花朵 <xiaoqiangclub@hotmail.com>
2024-06-14 08:49:12 +0800

    MessagePush

--- tests/test_senders.py
+++ tests/test_senders.py
@@ -15,29 +15,23 @@ async def test_senders():
     }
 
     wechat_args = {
-        "wechat_corp_id": "ww04df62e2c859b771",
-        "wechat_corp_secret": "y88b58_-eX7-1LXMW9vIV1L3DJbOQ3-XQC3c1CdQi1I",
-        "wechat_agent_id": 1000004,
-        # "to_user": "@XiaoqiangClub",
-        "message": "这是一条测试微信消息11"
+        "wechat_corp_id": "ww04df6259b771",
+        "wechat_corp_secret": "y88b58_-eX7-1LXMW9v3c1CdQi1I",
+        "wechat_agent_id": 1000001,
+        "message": "这是一条测试微信消息"
     }
 
     dd_args = {
-        "webhook_url": "https://oapi.dingtalk.com/robot/send?access_token=b5142d02f0a5dee6937f049961cf47bf046a46b9ed4dcd193cf2a6ababfd71bd",
+        "webhook_url": "https://oapi.dingtalk.com/robot/send?access_token=b5142d02f0a5dee6937f049961cf47bf04cf2a6ababfd71bd",
         "message": "你好"
 
     }
-    feishu_args = {
-        "webhook_url": "https://open.feishu.cn/open-apis/im/v1/messages",
-        "message": "你好"
 
-    }
     push_args = {
         "message": "这是一条测试消息",
-        "token":"4a8cc3c64e72457d8a8562005f02f908"
+        "token": "4a8cc3c64e7245a85f02f908"
     }
 
-    # await AsyncSender.send_messages(feishu_args=feishu_args)
     await AsyncSender.send_messages(pushplus_args=push_args)
 
 
@@ -52,6 +46,6 @@ async def test_senders_with_config():
     await AsyncSender.send_messages_with_config(config_path_json, message, title, url)
 
 
-# if __name__ == '__main__':
-#     asyncio.run(test_senders())
-    # asyncio.run(test_senders_with_config())
+if __name__ == '__main__':
+    asyncio.run(test_senders())
+    asyncio.run(test_senders_with_config())
