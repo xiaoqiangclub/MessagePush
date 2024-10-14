98d2768b3bef2f84e49237de011c05cc32d81846
祖国滴°花朵 <xiaoqiangclub@hotmail.com>
2024-10-14 18:58:56 +0800

    修正github项目地址

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     description='A Python package for sending messages via various platforms asynchronously.',
     long_description=open('README.md', 'r', encoding='utf-8').read(),
     long_description_content_type='text/markdown',
-    url='https://github.com/yourusername/MessagePush',
+    url='https://github.com/xiaoqiangclub/MessagePush',
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
